07d374162495d1ceed2d72b6b1232e659edc6fa5
Marco Silva <marcosilva2203@outlook.pt>
2024-11-09 18:27:19 +0000

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -31,23 +31,23 @@ project "glfw"
         systemversion "latest"
 
         files
-		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_module.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
-		}
+        {
+            "src/win32_init.c",
+            "src/win32_joystick.c",
+            "src/win32_module.c",
+            "src/win32_monitor.c",
+            "src/win32_time.c",
+            "src/win32_thread.c",
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
+        }
 
         defines
         { 
-        	"_GLFW_WIN32",
-        	"_CRT_SECURE_NO_WARNINGS"
+            "_GLFW_WIN32",
+            "_CRT_SECURE_NO_WARNINGS"
         }
 
     filter "system:linux"
@@ -55,20 +55,21 @@ project "glfw"
         systemversion "latest"
 
         files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_module.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/posix_module.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
+        {
+            "src/x11_init.c",
+            "src/x11_monitor.c",
+            "src/x11_window.c",
+            "src/xkb_unicode.c",
+            "src/posix_module.c",
+            "src/posix_poll.c",
+            "src/posix_time.c",
+            "src/posix_thread.c",
+            "src/posix_module.c",
+            "src/glx_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c",
+            "src/linux_joystick.c"
+        }
 
         defines
         {
